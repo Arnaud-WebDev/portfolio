0d90b6e87fd55630c554107cdbaa7eba708c15f4
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2023-04-24 18:03:38 +0200

    Configuration du curseur en mode desktop

--- js/app.js
+++ js/app.js
@@ -16,3 +16,38 @@ window.addEventListener("scroll", () => {
   navbar.classList.remove("active")
   menu.classList.remove("toggle")
 })
+
+/* Selection du curseur */
+const cursor = document.querySelector(".cursor")
+const cursorDot = document.querySelector(".cursor-dot")
+let navLinks = document.querySelectorAll(".navbar a")
+
+/* Prend la position de la souris et la donne au curseur grâce à top et left */
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute("style", "top:" + (e.clientY - 15) + "px; left:" + (e.clientX - 15) + "px;")
+  cursorDot.setAttribute("style", "top:" + (e.clientY - 2.5) + "px; left:" + (e.clientX - 2.5) + "px;")
+})
+
+/* Permet d'ajouter la classe au curseur crée dans le CSS pour faire grossir le curseur au click */
+document.body.addEventListener("click", () => {
+  cursor.classList.add("expand")
+
+  setTimeout(() => {
+    cursor.classList.remove("expand")
+  }, 200)
+})
+
+/* Permet de faire grossir le curseur des liens quand on passe la souris dessus en ajoutant et retirant une classe */
+
+navLinks.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    cursor.classList.add("link-grow")
+    cursorDot.classList.add("dot-white")
+    link.classList.add("hovered-link")
+  })
+  link.addEventListener("mouseleave", () => {
+    cursor.classList.remove("link-grow")
+    cursorDot.classList.remove("dot-white")
+    link.classList.remove("hovered-link")
+  })
+})
