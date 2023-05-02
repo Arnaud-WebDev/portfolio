224f8a42581de374b5ec9956b08e4cbe5ac18ef7
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2023-05-02 16:17:36 +0200

    Text animé en JS dans la partie About

--- js/app.js
+++ js/app.js
@@ -149,3 +149,30 @@ btnToggle.addEventListener("click", () => {
     body.classList.remove("light")
   }
 })
+
+/***************** ANIMATED TEXT ********************/
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  animate_text()
+})
+
+function animate_text() {
+  let delay = 40,
+    delay_start = 0,
+    contents,
+    letters
+
+  document.querySelectorAll(".animated-text").forEach(function (elem) {
+    contents = elem.textContent.trim()
+    elem.textContent = ""
+    letters = contents.split("")
+    elem.style.visibility = "visible"
+
+    letters.forEach(function (letter, index_1) {
+      setTimeout(function () {
+        elem.textContent += letter
+      }, delay_start + delay * index_1)
+    })
+    delay_start += delay * letters.length
+  })
+}
